df41e64acfb8d089576f13e103323943842eb098
Marek Vavrečan <vavrecan@gmail.com>
2014-01-27 18:18:40 +0100

    Added default timezone for bootstrap

--- api/bootstrap.php
+++ api/bootstrap.php
@@ -4,6 +4,8 @@ define("APP_DIR", __DIR__);
 define("LIBS_DIR", __DIR__ . "/libs");
 define("TEMP_DIR", __DIR__ . "/temp");
 
+date_default_timezone_set("Europe/Berlin");
+
 require_once(__DIR__ . "/include/api.php");
 
 $config = include("config.php");
--- www/app/bootstrap.php
+++ www/app/bootstrap.php
@@ -5,6 +5,8 @@ define("LIBS_DIR", __DIR__ . "/libs");
 define("TEMPLATES_DIR", __DIR__ . "/templates");
 define("TEMP_DIR", __DIR__ . "/temp");
 
+date_default_timezone_set("Europe/Berlin");
+
 require(__DIR__ . "/include/site.php");
 
 $config = include("config.php");
